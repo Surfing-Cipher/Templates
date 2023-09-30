0dcb79e964e970bc1cd79b263374b03cfec879fb
Abduulah <abdullahzulfiqar068@gmail.com>
2023-09-30 16:03:33 +0500

    Form v.5

--- Template1.js
+++ Template1.js
@@ -1,46 +1,119 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
+    const formLogin = document.querySelector('#loginForm');
+    const formRegister = document.querySelector('#registerForm');
+    const formforgot = document.querySelector('#forgotForm');
+    const forgotpassword = document.querySelectorAll('#forgot');
     const registerLinks = document.querySelectorAll('#registerLink');
-    const backToLogin = document.querySelector('#backToLogin');
+    const backToLogin = document.querySelectorAll('#backToLogin');
     const loginPanel = document.querySelector('.wrapper');
     const registerPanel = document.querySelector('.register-panel');
+    const forgotPanel = document.querySelector('.forgot-password-panel');
 
     function showLoginPanel() {
         loginPanel.style.display = 'block';
         registerPanel.style.display = 'none';
+        forgotPanel.style.display = 'none';
     }
 
     function showRegisterPanel() {
         loginPanel.style.display = 'none';
-        console.log("hehe");
         registerPanel.style.display = 'block';
-        console.log("hehe2");
+        forgotPanel.style.display = 'none';
     }
 
-    form.addEventListener('submit', function(e) {
+    function showForgotPanel() {
+        loginPanel.style.display = 'none';
+        registerPanel.style.display = 'none';
+        forgotPanel.style.display = 'block'; 
+    }
+
+    formLogin.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const formData = new FormData(formLogin);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    });
+
+    formRegister.addEventListener('submit', function (e) {
         e.preventDefault();
 
-        const username = form.querySelector('input[type="text"]').value;
-        const password = form.querySelector('input[type="password"]').value;
+        const password1 = formRegister.querySelector('input[name="Password"]');
+        const password2 = formRegister.querySelector('input[name="Re-enterPassword"]');
 
-        // Add your authentication logic here
-        // For example, you can send the username and password to a server for validation
+        if (password1.value !== password2.value) {
+            alert("Passwords do not match");
+            return;
+        }
 
-        // If authentication is successful, you can redirect the user or perform other actions
+        const formData = new FormData(formRegister);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify(data),
+        })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
     });
+
+    formforgot.addEventListener('submit', function(e) {
+        e.preventDefault();
+        formforgot.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const formData = new FormData(formforgot);
+        const email = formData.get('Email');
+
+        /*http://localhost:3000/reset-password*/
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email}),
+            })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .then((data) => {
+            if (data.success) {
+                alert(`Your new password is: ${data.newPassword}`);
+            } else {
+                alert('Failed to reset password. Please try again later.');
+            }
+        })
+        .catch((error) => console.error('Error:', error));
+    });
+
+    })
+
+
+
 
     registerLinks.forEach(function(link) {
         link.addEventListener('click', function(e) {
             e.preventDefault();
-            console.log("hehe3");
             showRegisterPanel();
         });
     });
 
-    backToLogin.addEventListener('click', function(e) {
-        e.preventDefault();
-        console.log("hehe4");
-        showLoginPanel();
+    backToLogin.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            showLoginPanel();
+        });
+    });
+
+    forgotpassword.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            showForgotPanel();
+        });
     });
 });
-
